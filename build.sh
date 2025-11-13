18e5e7f5d1740b82455b3212c0b44ee75a40164b
zackyape <75405032+zackyape@users.noreply.github.com>
2025-11-13 22:00:59 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,15 +3,33 @@
 #removals
 rm -rf .repo/local_manifests
 
-#sync
+# Remove tree
+rm -rf device/xiaomi/vayu
+rm -rf hardware/xiaomi
+rm -rf vendor/xiaomi/vayu
+rm -rf kernel/xiaomi/vayu
+rm -rf vendor/xiaomi/vayu-miuicamera
+
+#init
 repo init -u https://github.com/ArrowOS-T/android_manifest.git -b arrow-13.1_ext --git-lfs --depth=1
+echo "=================="
+echo "Repo init success"
+echo "=================="
+#clone local
 git clone https://github.com/zackyape/scripts -b arrow .repo/local_manifests
+echo "=================="
+echo "Local manifests clone success"
+echo "=================="
+
+#sync
 if [ -f /opt/crave/resync.sh ]; then
   /opt/crave/resync.sh
 else
   repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 fi
-
+echo "=================="
+echo "Sync success"
+echo "=================="
 # custom repos
 # rm -rf vendor/lineage-priv/keys
 # git clone --depth=1 https://github.com/pure-soul-kk/keys vendor/lineage-priv/keys
