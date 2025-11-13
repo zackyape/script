79b6b2bcaa872c32cb72df3ed394e931c1317b49
zackyape <75405032+zackyape@users.noreply.github.com>
2025-11-13 22:13:18 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,11 +4,13 @@
 rm -rf .repo/local_manifests
 
 # Remove tree
+rm -rf prebuilts/clang/host/linux-x86
 rm -rf device/xiaomi/vayu
 rm -rf hardware/xiaomi
 rm -rf vendor/xiaomi/vayu
 rm -rf kernel/xiaomi/vayu
 rm -rf vendor/xiaomi/vayu-miuicamera
+rm -rf packages/apps/ViPER4AndroidFX
 
 #init
 repo init -u https://github.com/ArrowOS-T/android_manifest.git -b arrow-13.1_ext --git-lfs --depth=1
@@ -17,6 +19,8 @@ echo "Repo init success"
 echo "=================="
 #clone local
 git clone https://github.com/zackyape/scripts -b arrow .repo/local_manifests
+git clone https://github.com/kdrag0n/proton-clang --depth 1  prebuilts/clang/host/linux-x86/clang-proton
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
 echo "=================="
 echo "Local manifests clone success"
 echo "=================="
